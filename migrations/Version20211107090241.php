c23388495ef43a1199461ba13c07135761aad5af
{iurii.troinikov} <yurkel12@gmail.com>
2021-11-07 12:43:35 +0200

    Lesson5: Added Endpoint: create note, category.

--- migrations/Version20211107090241.php
+++ migrations/Version20211107090241.php
@@ -7,25 +7,31 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
+
 final class Version20211107090241 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Create Category Entity';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(100) DEFAULT NULL, notes VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+
+        $this->addSql('
+        CREATE TABLE category (
+            id INT AUTO_INCREMENT NOT NULL, 
+            title VARCHAR(100) DEFAULT NULL, 
+            notes VARCHAR(255) DEFAULT NULL, 
+            PRIMARY KEY(id)
+           ) 
+                DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+           ');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
+
         $this->addSql('DROP TABLE category');
     }
 }
--- src/Controller/ChecklistController.php
+++ src/Controller/ChecklistController.php
@@ -20,21 +20,21 @@ class ChecklistController extends AbstractController
 
     public function __construct(NoteRepository $noteRepository)
     {
-         $this->noteRepository = $noteRepository;
+        $this->noteRepository = $noteRepository;
     }
 
     private array $categories = [
-       1 => [
-          'title' => 'My summer weekends',
-           'notes' => [1, 2, 3]
-       ],
+        1 => [
+            'title' => 'My summer weekends',
+            'notes' => [1, 2, 3]
+        ],
         2 => [
             'title' => 'My favorite books review',
-             'notes' => [4, 5, 6]
+            'notes' => [4, 5, 6]
         ],
         3 => [
             'title' => 'My friends hobbies',
-             'notes' => [7, 8, 9]
+            'notes' => [7, 8, 9]
         ]
 
     ];
@@ -46,55 +46,55 @@ class ChecklistController extends AbstractController
             'text' => 'Loren ipsun 1',
             'category_id' => 1
         ],
-     2 => [
+        2 => [
             'id' => 2,
             'title' => 'Some note 2',
             'text' => 'Loren ipsun 2',
             'category_id' => 1
-     ],
-     3 => [
+        ],
+        3 => [
             'id' => 3,
             'title' => 'Some note 3',
             'text' => 'Loren ipsun 3',
             'category_id' => 1
 
         ],
-     4 => [
+        4 => [
             'id' => 4,
             'title' => 'Some note 4',
             'text' => 'Loren ipsun 4',
             'category_id' => 2
 
         ],
-     5 => [
+        5 => [
             'id' => 5,
             'title' => 'Some note 5',
             'text' => 'Loren ipsun 5',
             'category_id' => 2
 
         ],
-     6 => [
+        6 => [
             'id' => 6,
             'title' => 'Some note 6',
             'text' => 'Loren ipsun 6',
             'category_id' => 2
 
         ],
-     7 => [
+        7 => [
             'id' => 7,
             'title' => 'Some note 7',
             'text' => 'Loren ipsun 7',
             'category_id' => 3
 
         ],
-     8 => [
+        8 => [
             'id' => 8,
             'title' => 'Some note 8',
             'text' => 'Loren ipsun 8',
             'category_id' => 3
 
         ],
-     9 => [
+        9 => [
             'id' => 9,
             'title' => 'Some note 9',
             'text' => 'Loren ipsun 9',
@@ -111,8 +111,8 @@ class ChecklistController extends AbstractController
     public function listAll(): Response
     {
 
-      return $this->render('checklist/list.html.twig', [
-          'notes' => $this->notes,
+        return $this->render('checklist/list.html.twig', [
+            'notes' => $this->notes,
         ]);
     }
 
@@ -121,18 +121,18 @@ class ChecklistController extends AbstractController
      */
     public function listByCategory(string $categoryId): Response
     {
-        if (!isset($this->categories[(int) $categoryId])) {
+        if (!isset($this->categories[(int)$categoryId])) {
             throw new Exception('You ask for category that not exists');
         }
 
-        $category = $this->categories[(int) $categoryId] ?? null;
+        $category = $this->categories[(int)$categoryId] ?? null;
         $notesIds = $category['notes'];
 
         $notes = array_filter($this->notes, function (array $note) use ($notesIds) {
             return in_array($note['id'], $notesIds, true);
         });
 
-           return $this->render('checklist/list.html.twig', [
+        return $this->render('checklist/list.html.twig', [
             'notes' => $notes
         ]);
     }
@@ -151,19 +151,20 @@ class ChecklistController extends AbstractController
         $notesIds = $category['notes'];
 
         $notes = array_filter($this->notes, function (array $note) use ($notesIds) {
-            return in_array($note['id'], $notesIds, true) ;
+            return in_array($note['id'], $notesIds, true);
         });
         if (!isset($notes[(int)$noteId])) {
             throw new Exception('There is no note in selected category');
         }
 
 
-        $note = $notes[(int) $noteId];
+        $note = $notes[(int)$noteId];
 
         return $this->render('checklist/get.html.twig', [
             'note' => $note
         ]);
     }
+
     /**
      * @Route("/create", name="create")
      */
@@ -182,14 +183,13 @@ class ChecklistController extends AbstractController
         $notes = $this->noteRepository->findAll();
 
 
-
         return $this->render('checklist/list.html.twig', [
             'notes' => $notes,
         ]);
     }
 
     /**
-     * @Route("/delete_note/{id}", name="create")
+     * @Route("/delete_note/{id}", name="delete_note")
      */
 
     public function deleteAction(int $id): Response
@@ -204,29 +204,38 @@ class ChecklistController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/create_note/{id}", name="create_note")
+     */
 
-//  /**
-    //  * @Route("/create_category", name="create_category")
-    //  */
+    public function createNoteAction(int $id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $noteToCreate = $this->noteRepository->find($id);
+        $entityManager->remove($noteToCreate);
+        $entityManager->flush();
 
-    // public function createCategoryAction(): Response
-    // {
-    //    $entityManager = $this->getDoctrine()->getManager();
-    //   $newCategory = new Category();
-    //    $newCategory
-    //       ->setTitle('New Category title')
-    //       ->setNotes('New Category note');
-
-    //   $entityManager->persist($newCategory);
-    //   $entityManager->flush();
-
-    //  $categories = $this->categoryRepository->findAll();
+        return $this->render('checklist/create_note.html.twig', [
+            'id' => $id,
+        ]);
+    }
 
 
-    //   return $this->render('checklist/list.html.twig', [
-    //      'categories' => $categories,
-    //  ]);
-    // }
+    /**
+     * @Route("/create_category/{id}", name="create_category")
+     */
 
+    public function createCategoryAction(int $id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $categoryToCreate = $this->categoryRepository->find($id);
+        $entityManager->remove($categoryToCreate);
+        $entityManager->flush();
+
+        return $this->render('checklist/create_category.html.twig', [
+            'id' => $id,
+        ]);
+    }
 }
+
 
