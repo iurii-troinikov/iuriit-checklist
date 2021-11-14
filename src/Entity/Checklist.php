81d833821fd81060568d6b95b602ba93bc0ec918
{iurii.troinikov} <yurkel12@gmail.com>
2021-11-14 10:05:38 +0200

    Lesson-6: Implemented Checklist Entity. Remove useless code.

--- src/Entity/Checklist.php
+++ src/Entity/Checklist.php
@@ -1,12 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Entity;
 
 use App\Repository\ChecklistRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass=ChecklistRepository::class)
+ * @ORM\Entity(repositoryClass="App\Repository\ChecklistRepository", repositoryClass=ChecklistRepository::class)
  */
 class Checklist
 {
@@ -15,19 +17,25 @@ class Checklist
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
-    private $id;
+    private int $id;
+
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $title;
+    private string $title;
+
+    public function __construct(string $title)
+    {
+        $this->title = $title;
+    }
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getTitle(): ?string
+    public function getTitle(): string
     {
         return $this->title;
     }
--- src/Repository/ChecklistRepository.php
+++ src/Repository/ChecklistRepository.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Repository;
 
 use App\Entity\Checklist;
@@ -19,32 +21,4 @@ class ChecklistRepository extends ServiceEntityRepository
         parent::__construct($registry, Checklist::class);
     }
 
-    // /**
-    //  * @return Checklist[] Returns an array of Checklist objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('c.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Checklist
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
