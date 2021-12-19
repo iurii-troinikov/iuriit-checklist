9c4b5988d0a9cf912cff38f1f818c79d1bb72036
{iurii.troinikov} <yurkel12@gmail.com>
2021-12-19 10:01:43 +0200

    Lesson-11 'Events, Listeners': Create admin command improvements.

--- src/Command/CreateAdminCommand.php
+++ src/Command/CreateAdminCommand.php
@@ -7,8 +7,8 @@ use App\Service\UserService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 class CreateAdminCommand extends Command
@@ -23,24 +23,19 @@ class CreateAdminCommand extends Command
         $this->userService = $userService;
         $this->em = $em;
     }
-    protected function configure(): void
-    {
-        $this
-            ->addOption('username', 'u', InputOption::VALUE_REQUIRED, 'User name')
-            ->addOption('password', 'p', InputOption::VALUE_REQUIRED, 'User password')
-        ;
-    }
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $helper = $this->getHelper('question');
+        $question1 = new Question('Please enter admin user name:');
+        $question2 = new Question('Please enter admin password:');
+        $userName = $helper->ask($input, $output, $question1);
+        $password = $helper->ask($input, $output, $question2);
         $io = new SymfonyStyle($input, $output);
-        $userName = $input->getOption('username');
-        $password = $input->getOption('password');
         try {
             $user = $this->userService->create($password, $userName);
             $user->addRole(RolesEnum::ADMIN);
             $this->em->persist($user);
             $this->em->flush();
-
             $io->success('Admin user successfully created');
         } catch (\Exception $e) {
             $io->error($e->getMessage());
