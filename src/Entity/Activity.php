61230197d4c2adabe6675651e7848180483525a0
{iurii.troinikov} <yurkel12@gmail.com>
2021-12-19 13:32:14 +0200

    Lesson-11 'Events, Listeners': Added Activity constructor.

--- src/Entity/Activity.php
+++ src/Entity/Activity.php
@@ -16,7 +16,7 @@ class Activity
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
-    private $id;
+    private ?int $id = null;
 
     /**
      * @ORM\Column(type="string", length=10)
