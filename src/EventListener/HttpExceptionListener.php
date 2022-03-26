6181f43035210af16d6a0e394cc9cec7737fd7bf
iurii-troinikov <yurkel12@gmail.com>
2022-03-26 16:07:38 +0200

    Lesson 20-API Implementation: Update HttpExceptionListener for API

--- src/EventListener/HttpExceptionListener.php
+++ src/EventListener/HttpExceptionListener.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\EventListener;
 
 use App\Enum\FlashMessagesEnum;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
@@ -12,21 +14,35 @@ class HttpExceptionListener
 {
     public function onKernelException(ExceptionEvent $event)
     {
+        $request = $event->getRequest();
+        if (strpos($request->getRequestUri(), '/api/') === 0) {
+            $this->handleApiException($event);
+            return;
+        }
+
         $exception = $event->getThrowable();
         if (!$exception instanceof HttpExceptionInterface) {
             return;
         }
+
         $request = $event->getRequest();
         $session = $request->getSession();
         if (!$refererUrl = $request->headers->get('referer')) {
             $refererUrl = '/';
         }
-
         $response = new RedirectResponse($refererUrl);
         $response->setStatusCode($exception->getStatusCode());
         $response->headers->replace($exception->getHeaders());
-
         $session->getFlashBag()->add(FlashMessagesEnum::FAIL, $exception->getMessage());
         $event->setResponse($response);
+    }
+
+    private function handleApiException(ExceptionEvent $event): void {
+        $exception = $event->getThrowable();
+        $message = $exception instanceof HttpExceptionInterface
+            ? $exception->getMessage()
+            : 'Something went wrong ... Try later';
+
+        $event->setResponse(new JsonResponse(['error' => $message]));
     }
 }
