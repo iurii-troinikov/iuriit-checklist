054e36b2f28a576b597495479a18d641d78e3a7d
iurii-troinikov <yurkel12@gmail.com>
2022-04-06 15:55:50 +0300

    Lesson 22-Testing: Updated HTTP exception listener

--- src/EventListener/HttpExceptionListener.php
+++ src/EventListener/HttpExceptionListener.php
@@ -29,7 +29,7 @@ class HttpExceptionListener
             return;
         }
         $exception = $event->getThrowable();
-        if (!$exception instanceof ValidationException || !$exception instanceof NotFoundHttpException) {
+        if (!$exception instanceof ValidationException) {
             return;
         }
         $session = $request->getSession();
@@ -37,9 +37,13 @@ class HttpExceptionListener
             $refererUrl = '/';
         }
         $response = new RedirectResponse($refererUrl);
-        $response->setStatusCode($exception->getStatusCode());
-        $response->headers->replace($exception->getHeaders());
-        $session->getFlashBag()->add(FlashMessagesEnum::FAIL, $exception->getMessage());
+        $response->setStatusCode($exception->getCode());
+        foreach ($exception->getErrorsList() as $error) {
+            $session->getFlashBag()->add(FlashMessagesEnum::FAIL, $error->getMessage());
+        }
+        if ($exception->getMessage()) {
+            $session->getFlashBag()->add(FlashMessagesEnum::FAIL, $exception->getMessage());
+        }
         $event->setResponse($response);
     }
     private function handleApiException(ExceptionEvent $event): void
