7304cadb3ddcaa708d059864c25cbed33b4944c0
iurii-troinikov <yurkel12@gmail.com>
2022-04-11 10:34:20 +0300

    Lesson-8: fix migrations

--- migrations/Version20211106122312.php
+++ migrations/Version20211106122312.php
@@ -12,14 +12,14 @@ final class Version20211106122312 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Create Note entity';
+        return 'Create Todo entity';
     }
 
     public function up(Schema $schema): void
     {
 
         $this->addSql('
-           CREATE TABLE note (
+           CREATE TABLE to_do (
                id INT AUTO_INCREMENT NOT NULL, 
                title VARCHAR(100) NOT NULL, 
                text LONGTEXT NOT NULL, 
@@ -32,6 +32,6 @@ final class Version20211106122312 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE note');
+        $this->addSql('DROP TABLE to_do');
     }
 }
