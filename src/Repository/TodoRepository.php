2941d5b382d63a5bfbfffc854174efcc84399851
iurii-troinikov <yurkel12@gmail.com>
2022-04-11 16:13:08 +0300

    Lesson-4: fix code

--- src/Repository/TodoRepository.php
+++ src/Repository/TodoRepository.php
@@ -12,39 +12,10 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method Todo[]    findAll()
  * @method Todo[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class todoRepository extends ServiceEntityRepository
+class TodoRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Todo::class);
     }
-
-    // /**
-    //  * @return todo[] Returns an array of todo objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('n')
-            ->andWhere('n.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('n.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?todo
-    {
-        return $this->createQueryBuilder('n')
-            ->andWhere('n.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
