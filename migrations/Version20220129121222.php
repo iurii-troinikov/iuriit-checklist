618e4ea2421cfa3ccb333487783658219f16bfa2
iurii-troinikov <yurkel12@gmail.com>
2022-04-12 15:34:11 +0300

    Lesson-20: fix code

--- migrations/Version20220129121222.php
+++ migrations/Version20220129121222.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 declare(strict_types=1);
 
 namespace DoctrineMigrations;
@@ -21,4 +21,4 @@ final class Version20220129121222 extends AbstractMigration
         $this->addSql('ALTER TABLE activity DROP FOREIGN KEY FK_AC74095A5BE9ECD7');
         $this->addSql('ALTER TABLE activity ADD CONSTRAINT FK_AC74095A5BE9ECD7 FOREIGN KEY (to_do_id) REFERENCES to_do (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
     }
-}
+}*/
