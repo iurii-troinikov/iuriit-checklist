4d198d4db68c090b8f44cc3ecaea09c1c857a2f4
iurii-troinikov <yurkel12@gmail.com>
2022-04-17 10:47:54 +0300

    Lesson-9: migrations updates

--- migrations/Version20220417074207.php
+++ migrations/Version20220417074207.php
@@ -7,26 +7,25 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20211204083042 extends AbstractMigration
+final class Version20220417074207 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
-        $this->addSql('DELETE FROM to_do');
-        $this->addSql('ALTER TABLE to_do ADD user_id INT NOT NULL');
-        $this->addSql('ALTER TABLE to_do ADD CONSTRAINT FK_1249EDA0A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
-        $this->addSql('CREATE INDEX IDX_1249EDA0A76ED395 ON to_do (user_id)');
-        $this->addSql('DELETE FROM checklist');
         $this->addSql('ALTER TABLE checklist ADD user_id INT NOT NULL');
         $this->addSql('ALTER TABLE checklist ADD CONSTRAINT FK_5C696D2FA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('CREATE INDEX IDX_5C696D2FA76ED395 ON checklist (user_id)');
+        $this->addSql('ALTER TABLE to_do ADD user_id INT NOT NULL');
+        $this->addSql('ALTER TABLE to_do ADD CONSTRAINT FK_1249EDA0A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_1249EDA0A76ED395 ON to_do (user_id)');
     }
+
     public function down(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE to_do DROP FOREIGN KEY FK_1249EDA0A76ED395');
-        $this->addSql('DROP INDEX IDX_1249EDA0A76ED395 ON to_do');
-        $this->addSql('ALTER TABLE to_do DROP user_id');
         $this->addSql('ALTER TABLE checklist DROP FOREIGN KEY FK_5C696D2FA76ED395');
         $this->addSql('DROP INDEX IDX_5C696D2FA76ED395 ON checklist');
         $this->addSql('ALTER TABLE checklist DROP user_id');
+        $this->addSql('ALTER TABLE to_do DROP FOREIGN KEY FK_1249EDA0A76ED395');
+        $this->addSql('DROP INDEX IDX_1249EDA0A76ED395 ON to_do');
+        $this->addSql('ALTER TABLE to_do DROP user_id');
     }
 }
