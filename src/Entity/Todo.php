b7d08573376a77738c16e17dbf92ebb05ee57f1f
iurii-troinikov <yurkel12@gmail.com>
2022-04-17 09:34:08 +0300

    Lesson-4: migrations updates

--- src/Entity/Todo.php
+++ src/Entity/Todo.php
@@ -16,41 +16,19 @@ class Todo
      * @ORM\Column(type="integer")
      */
     private int $id;
-
-    /**
-     * @var string
-     * @ORM\Column(type="string", length=100)
-     */
-    private string $title;
-
     /**
      * @var string
      * @ORM\Column(type="text")
      */
     private string $text;
-
     public function getId(): int
     {
         return $this->id;
     }
-
-    public function getTitle(): string
-    {
-        return $this->title;
-    }
-
-    public function setTitle(string $title): self
-    {
-        $this->title = $title;
-
-        return $this;
-    }
-
     public function getText(): string
     {
         return $this->text;
     }
-
     public function setText(string $text): self
     {
         $this->text = $text;
