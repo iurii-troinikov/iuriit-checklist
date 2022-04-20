59167f3cdee423eed0d988524586cdc134161453
iurii-troinikov <yurkel12@gmail.com>
2022-04-20 16:25:31 +0300

    Lesson-12: fix migrations

--- migrations/Version20211114082008.php
+++ migrations/Version20211114082008.php
@@ -7,9 +7,15 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
+
 final class Version20211114082008 extends AbstractMigration
 {
- function up(Schema $schema): void
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
     {
 
         $this->addSql('ALTER TABLE to_do ADD checklist_id INT NOT NULL');
